f51a699d342c80a957ec8471bad378d8fe2b7739
Daniel Flower <git@danielflower.com>
2016-05-28 23:57:45 +0800

    Deployed apps can now be proxied!!!

--- src/main/java/com/danielflower/apprunner/router/web/ReverseProxy.java
+++ src/main/java/com/danielflower/apprunner/router/web/ReverseProxy.java
@@ -10,6 +10,7 @@ import org.slf4j.LoggerFactory;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URL;
 import java.util.regex.Matcher;
@@ -47,27 +48,29 @@ public class ReverseProxy extends AsyncProxyServlet {
 
     protected String rewriteTarget(HttpServletRequest clientRequest) {
         String uri = clientRequest.getRequestURI();
+        String query = isEmpty(clientRequest.getQueryString()) ? "" : "?" + clientRequest.getQueryString();
+
         log.info(clientRequest.getMethod() + " " + uri);
         Matcher matcher = APP_REQUEST.matcher(uri);
         if (uri.startsWith("/api/")) {
             if (uri.equals("/api/v1/apps")) {
                 String method = clientRequest.getMethod().toUpperCase();
                 if (method.equals("POST")) {
-                    return cluster.getRunners().get(0).url.resolve(uri).toString();
+                    URI targetAppRunner = cluster.getRunners().get(0).url;
+                    return targetAppRunner.resolve(uri) + query;
                 }
             } else {
                 Matcher appApiMatcher = APP_API.matcher(uri);
                 if (appApiMatcher.matches()) {
                     String prefix = appApiMatcher.group(1);
                     // TODO: proxy to app runner
-                    return cluster.getRunners().get(0).url.resolve(uri).toString();
+                    return cluster.getRunners().get(0).url.resolve(uri) + query;
                 }
             }
         } else if (matcher.matches()) {
             String prefix = matcher.group(1);
             URL url = proxyMap.get(prefix);
             if (url != null) {
-                String query = isEmpty(clientRequest.getQueryString()) ? "" : "?" + clientRequest.getQueryString();
                 String newTarget = url.toString() + matcher.group(2) + query;
                 log.info("Proxying to " + newTarget);
                 return newTarget;
@@ -76,6 +79,23 @@ public class ReverseProxy extends AsyncProxyServlet {
 
         log.info("No proxy target configured for " + uri);
         return null;
+    }
+
+    @Override
+    protected void onProxyResponseSuccess(HttpServletRequest clientRequest, HttpServletResponse proxyResponse, Response serverResponse) {
+        super.onProxyResponseSuccess(clientRequest, proxyResponse, serverResponse);
+
+        if (clientRequest.getMethod().equals("POST") && clientRequest.getRequestURI().equals("/api/v1/apps")) {
+            String appName = proxyResponse.getHeader("Location");
+            appName = appName.substring(appName.lastIndexOf("/") + 1);
+            try {
+                URI targetAppRunner = cluster.getRunners().get(0).url;
+                proxyMap.add(appName, targetAppRunner.resolve("/" + appName).toURL());
+            } catch (MalformedURLException e) {
+                log.error("Could not write proxy value", e);
+            }
+        }
+
     }
 
     protected void onProxyRewriteFailed(HttpServletRequest clientRequest, HttpServletResponse proxyResponse) {
