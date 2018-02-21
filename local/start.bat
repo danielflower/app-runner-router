1a29df181dd9ccb32442468a7883ab8dd081601f
Daniel Flower <git@danielflower.com>
2018-02-21 20:13:46 +0800

    Fixed local startup scripts

--- local/start.bat
+++ local/start.bat
@@ -1,2 +1,2 @@
 @echo off
-java -Dlogback.configurationFile=logback.xml -jar ..\target\app-runner-router-1.1-SNAPSHOT.jar config.properties
+java -Dlogback.configurationFile=logback.xml -jar ..\target\app-runner-router-1.7-SNAPSHOT.jar config.properties
